414fc4c5f4e611bf26f586c5b1bc71c6993daaa9
Bruno <bknifepicarth@gmail.com>
2022-06-24 19:00:52 -0300

    teste commit

---
+++ email.php
@@ -0,0 +1,23 @@
+<?php
+
+
+if (isset ($_POST(['email']) && !empty($_POST(['email'])){
+
+$nome = addslashes ($_POST(['name']))
+$email = addslashes ($_POST(['email']))
+$idade= addslashes ($_POST(['idade']))
+$quantidade = addslashes ($_POST(['quantidade']))
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
